2c4e3ffad62d5e23c1f489a4a58d43c0c35f9df7
Aaron Aung <aaronaung.95@gmail.com>
2018-06-03 16:45:05 -0700

    Server script now responds with an index to flip

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+const NUM_IMAGES = 10;
+
 let count = 0;
+let flipped = [];
+let unflipped = Array.apply(null, {length: NUM_IMAGES}).map((v, i) => i ); //initialize the unflipped to 1 - 100 
+
+function flip(nIndex){
+    //edge cases
+    switch(nIndex){
+        case -1:
+            return;
+        case (NUM_IMAGES-1):
+            flipped.push(unflipped[nIndex]);
+            unflipped = unflipped.slice(0, NUM_IMAGES-1);
+            return;
+        case 0:
+            flipped.push(unflipped[nIndex]);
+            unflipped = unflipped.slice(1, NUM_IMAGES);
+            return;
+    }
+    let firstHalf = unflipped.slice(0, nIndex);
+    let secondHalf = unflipped.slice(nIndex + 1, unflipped.length);
+    flipped.push(unflipped[nIndex]);
+    unflipped = [...firstHalf, ... secondHalf];
+}
 
 module.exports = (req, res) => {
-    count++;
-    return count;
+    if(req.url != "/favicon.ico"){
+        count++;
+        if(count <= NUM_IMAGES){
+            let randomIndex = Math.floor(Math.random() * unflipped.length);
+            console.log(randomIndex);
+            let toFlip = unflipped[randomIndex]
+            flip(randomIndex);
+            return "Index to be flipped: " + toFlip + "| Current #page loads: " + count + "| Current flipped indices: " + flipped + "| Current unflipped indices: " + unflipped; 
+        }
+        return "Everything is flipped!"
+    }
 }
